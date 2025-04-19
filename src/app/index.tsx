f8f0c001fa044afa13d2f1195171dbac1e2ad722
nguyenrise <hnguyen33@rrc.ca>
2025-04-19 15:40:39 -0500

    Update index.tsx

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,11 @@
 import AppProvider from "./provider";
 import AppRouter from "./router";
-import { ErrorBoundary } from "react-error-boundary";
 
 function App() {
   return (
-    <ErrorBoundary fallback={<div>Something went wrong!</div>}>
-      <AppProvider>
-        <AppRouter />
-      </AppProvider>
-    </ErrorBoundary>
+    <AppProvider>
+      <AppRouter />
+    </AppProvider>
   );
 }
 
